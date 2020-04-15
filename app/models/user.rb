c136e4b5d77ded6ccabe807114204131d17e82c1
Benoit Calin <benoit.calin@gmail.com>
2020-04-15 16:29:34 +0200

    finish chapter connecting nodes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  # has_many :posts
+  has_many :votes
+  has_many :links
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
